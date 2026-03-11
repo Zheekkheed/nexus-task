81776bdec53e8a7439521e6cb798dd17a613c5e2
Zheekkheed <74933343+Zheekkheed@users.noreply.github.com>
2026-03-11 09:09:30 +0100

    initial commit

--- js/jsconfig.js
+++ js/jsconfig.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-    apiKey: "YOUR_ACTUAL_API_KEY",
-    authDomain: "your-project.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef123456"
-};
+    apiKey: "AIzaSyACOfxM2xkBrbJ7WDWv6uo1AhRIbbrg3D0",
+    authDomain: "nexus-tasks-2fc15.firebaseapp.com",
+    projectId: "nexus-tasks-2fc15",
+    storageBucket: "nexus-tasks-2fc15.firebasestorage.app",
+    messagingSenderId: "1021983518867",
+    appId: "1:1021983518867:web:71b03c6f7782089ef29d66",
+    measurementId: "G-Z8NQ608L34"
+  };
